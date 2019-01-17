cfe417f4ac033a13a031d3f31956ee9131971ae3
Matthew Whaley <mjwhaley@mac.com>
2019-01-17 15:11:28 +0000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+
+//Creating the vars for the cards
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "King";
+var cardFour = "King";
+
+console.log("User flipped " + cardThree);

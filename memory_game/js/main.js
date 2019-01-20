7466e2f244741e9f88e2b57779908a17abd7e657
Matthew Whaley <mjwhaley@mac.com>
2019-01-20 18:52:43 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,27 +1,58 @@
-var cards = ["queen", "queen", "king", "king"];;
+console.log("Up and running!");
+var cards = [
+ {
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+ },
+ {
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+ },
+ {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
-var flipCard = function(cardId) {
-  console.log("User flipped" + " " + cards[cardId]);
-checkForMatch();
-cardsInPlay.push(cards[cardId]);
-};
-
 var checkForMatch = function() {
-// if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
-  } else {
-  alert("Sorry, try again.");}
-//}
+	if (cardsInPlay.length == 2) {
+  	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		alert("You found a match!");
+  	}
+  	else {
+  		alert("Sorry, try again.");
+  	}
+}
 };
 
+var flipCard = function() {
+	var cardId = parseInt(this.getAttribute('data-id'),10);
+	console.log("User flipped " + cards[cardId].rank);
+	this.src = cards[cardId].cardImage;
+  	cardsInPlay.push(cards[cardId].rank);
+  	checkForMatch();
+  	console.log(cards[cardId].cardImage);
+  	console.log(cards[cardId].suit);
+ };
 
+ var createBoard =function () {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+};
 
-flipCard(0);
-flipCard(2);
-
-
-//console.log("User flipped " + cardOne);
-//console.log("User flipped " + cardTwo);
-//console.log(cardsInPlay);
+createBoard();
